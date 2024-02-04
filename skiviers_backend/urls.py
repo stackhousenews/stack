9d0c238b3fe418dafa47d3ce33fdc3682f98a64b
web3research <jefferson.vieira@finance.gov.ie>
2024-02-04 18:05:30 +0000

    Entry Point

--- skiviers_backend/urls.py
+++ skiviers_backend/urls.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, include, re_path
+from .views import IndexTemplateView
 from account.views import activateemail
 
 urlpatterns = [
@@ -13,4 +13,5 @@ urlpatterns = [
     path('api/notifications/', include('notification.urls')),
     path('activateemail/', activateemail, name='activateemail'),
     path('admin/', admin.site.urls),
-] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r"^.*$", IndexTemplateView.as_view(), name="entry-point")
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
