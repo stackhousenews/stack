56a555ea1cafede1db0bbb05d2278ba22ab4c90d
web3research <jefferson.vieira@finance.gov.ie>
2024-02-04 21:32:45 +0000

    Vite

--- skiviers_frontend/vite.config.js
+++ skiviers_frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         main: path.resolve('./src/main.js'),
       },
       output: {
-        dir: '../staticfiles/',
+        dir: '../staticfiles/assets/',
         entryFileNames: 'main.js',
       },
     },
