02653e5fc449837143c61b05c24d6687d9d65e46
web3research <jefferson.vieira@finance.gov.ie>
2024-02-05 08:11:40 +0000

    Vite

--- skiviers_backend/settings.py
+++ skiviers_backend/settings.py
@@ -222,8 +222,9 @@ else:
     # https://docs.djangoproject.com/en/3.1/howto/static-files/
     MEDIA_URL = '/media/'   
     STATIC_URL = '/static/'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
