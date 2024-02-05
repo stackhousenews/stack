191751c3db5f560018f70c64734fd4218ed9dbd4
web3research <jefferson.vieira@finance.gov.ie>
2024-02-05 13:34:49 +0000

    Vite

--- skiviers_backend/settings.py
+++ skiviers_backend/settings.py
@@ -77,6 +77,9 @@ REST_FRAMEWORK = {
     )
 }
 
+if not DEBUG:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = ['rest_framework.renderers.JSONRenderer']
+
 if DEBUG:
 
     CORS_ALLOWED_ORIGINS = [
