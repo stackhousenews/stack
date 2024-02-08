cf4ff16876f320f73bae8b41933fb130c3bc4713
web3research <jefferson.vieira@finance.gov.ie>
2024-02-08 22:39:30 +0000

    Google

--- skiviers_backend/settings.py
+++ skiviers_backend/settings.py
@@ -85,7 +85,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    #'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.google',
     #'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.twitter',
     'crispy_forms',
