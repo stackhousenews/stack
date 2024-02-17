a6d57f0b627204cda0e621ade3c579ad7d844786
web3research <jefferson.vieira@finance.gov.ie>
2024-02-17 22:22:51 +0000

    Python upgrade

--- skiviers_backend/settings.py
+++ skiviers_backend/settings.py
@@ -163,18 +163,18 @@ TEMPLATES = [
     },
 ]
 
-SOCIALACCOUNT_PROVIDERS = {
-    'google': {
-        # For each OAuth based provider, either add a ``SocialApp``
-        # (``socialaccount`` app) containing the required client
-        # credentials, or list them here:
-        'APP': {
-            'client_id': '123',
-            'secret': '456',
-            'key': ''
-        }
-    }
-}
+#SOCIALACCOUNT_PROVIDERS = {
+#    'google': {
+#        # For each OAuth based provider, either add a ``SocialApp``
+#        # (``socialaccount`` app) containing the required client
+#        # credentials, or list them here:
+#        'APP': {
+#            'client_id': '123',
+#            'secret': '456',
+#            'key': ''
+#        }
+#    }
+#}
 
 
 
